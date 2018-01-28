3d83550651ddfc0b3bf3e883a25334d97dc5c5e3
SynneK1337 <276786@pw.edu.pl>
2018-01-28 03:57:24 +0100

    Simple CLI

---
+++ main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+print("Welcome to schoolMiner\n"
+      "Which coin you want to mine?\n"
+      "Available Coins:\n"
+      " - Ethereum (ETH)\n"
+      " - Monero (XMR)\n"
+      " - ZCash (ZEC)\n")
+
+coin = input("Coin: ")
+coin = coin.upper()
+
+if coin == "ZEC" or coin == "XMR":
+    method = input("CPU or GPU? ")
+    method = method.upper()
+
+if coin == "ETH":
+    eth()
+
+elif coin == "XMR" and method == "CPU":
+    xmr.cpu()
+
+elif coin == "XMR" and method == "GPU:
+    xmr.gpu()
+
+elif coin == "ZEC" and method == "CPU":
+    zec.cpu()
+
+elif coin == "ZEC" and method == "GPU":
+    zec.gpu()
