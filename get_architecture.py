0de1bb27c7e6eb6082b63ce7f34a1d27b38ba605
SynneK1337 <276786@pw.edu.pl>
2018-03-02 23:01:36 +0100

    Fix Polish windows architecture detecting

--- get_architecture.py
+++ get_architecture.py
@@ -3,13 +3,13 @@ from os import name
 
 def get_architecture():
     if name == "nt":
-        if check_output('wmic OS get OSArchitecture').decode('utf-8').replace('OSArchitecture', '').split()[0] == "64-bit":
-            return 64
-        else:
+        if check_output('wmic OS get OSArchitecture').decode('utf-8').replace('OSArchitecture', '').split()[0] == "32-bit" or check_output('wmic OS get OSArchitecture').decode('utf-8').replace('OSArchitecture', '').split()[0] == "32-bitowy":
             return 32
+        else:
+            return 64
 
     else:
-        if check_output(['uname', "-m"]).decode('utf-8') == "x86_64":
-            return 64
-        else:
+        if check_output(['uname', "-m"]).decode('utf-8') == "x86":
             return 32
+        else:
+            return 64
